0b78a1501f68d67a648a22c9283132362904dd5e
Gabriel Suassuna Almeida <36707258+GabrielSuassuna@users.noreply.github.com>
2018-03-24 09:10:03 -0300

    Update conversor.py

--- conversor.py
+++ conversor.py
@@ -1,29 +1,20 @@
-number = input('Digite um número de base 2, 10 ou 16:').upper()
+number = input('Digite um número de base 2, 10 ou 16:').upper ()
 base = input('O número digitado foi de base 2, 10 ou 16:')
+convert = int(input("Para qual base deseja converter: "))
 dicionary = {'A':10,"B":11,"C":12,"D":13,"E":14,"F":15,10:'A',11:'B',12:'C',13:"D",14:'E',15:'F'}
-def dectobin (x):
+def dectoall (x,y):
     x = int(x)
     binary =[]
     while x>0:
-        a = x%2
-        binary.append (a)
-        x = x//2
-    binary = binary [::-1]
-    b = ''.join(map(str, binary))
-    return b
-def dectohex (x):
-    x = int(x)
-    hexadecimal = []
-    while x>0:
-        a = x%16
+        a = x%y
         if a in dicionary:
             a = dicionary [a]
-            hexadecimal.append (a)
+            binary.append (a)
         else:
-            hexadecimal.append (a)
-        x = x//16
-    hexadecimal = hexadecimal[::-1]
-    b = ''.join(map(str, hexadecimal))
+            binary.append (a)
+        x = x//y
+    binary = binary [::-1]
+    b = ''.join(map(str, binary))
     return b
 def bintodec(x):
     y = len (x)
@@ -50,19 +41,25 @@ def hextodec(x):
         b =  b + decimal [indice]*(16**(y - indice - 1))
     return b
 if base == '10':
-    r1 = dectobin (number)
-    print ('Base 2: ','(', r1,')')
-    r2 = dectohex (number)
-    print ('Base 16: ','(', r2, ')')
+    r1 = dectoall (number, convert)
+    print ('Base', convert, ':', '(', r1,')')
 elif base =='2':
     r1 = bintodec (number)
-    print ('Base 10: ','(', r1,')')
-    r2 = dectohex (r1)
-    print ('Base 16: ','(', r2, ')')
+    r2 = dectoall (r1, convert)
+    if convert == 10:
+        print ('Base 10: ','(', r1,')')
+    elif convert == 16:
+        print ('Base 16: ','(', r2, ')')
+    else:
+        print ("Essa conversão não é possível")
 elif base =='16':
     r1 = hextodec (number)
-    print ('Base 10: ','(', r1,')')
-    r2 = dectobin (r1)
-    print ('Base 2: ','(', r2,')')
+    r2 = dectoall (r1, convert)
+    if convert == 10:
+        print ('Base 10: ','(', r1,')')
+    elif convert == 16:
+        print ('Base 2: ','(', r2, ')')
+    else:
+        print ("Essa conversão não é possível")
 else:
-    print('Base não reconhecida, tente outra vez.')
+    print ('Base não reconhecida, tente outra vez.')
