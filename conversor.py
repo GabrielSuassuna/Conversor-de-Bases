f2bae2e0ba67e94e20d5d8a8ff3a2066a8457d98
Gabriel Suassuna Almeida <36707258+GabrielSuassuna@users.noreply.github.com>
2018-03-24 08:54:50 -0300

    Update conversor.py

--- conversor.py
+++ conversor.py
@@ -1,4 +1,4 @@
-number = input('Digite um número de base 2, 10 ou 16:').upper
+number = input('Digite um número de base 2, 10 ou 16:').upper()
 base = input('O número digitado foi de base 2, 10 ou 16:')
 dicionary = {'A':10,"B":11,"C":12,"D":13,"E":14,"F":15,10:'A',11:'B',12:'C',13:"D",14:'E',15:'F'}
 def dectobin (x):
