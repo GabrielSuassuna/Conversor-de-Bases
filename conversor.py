39318fa6475e8d182c2956ff02444b84d0d4b24d
Gabriel Suassuna Almeida <36707258+GabrielSuassuna@users.noreply.github.com>
2018-03-24 09:19:59 -0300

    Update conversor.py

--- conversor.py
+++ conversor.py
@@ -1,8 +1,8 @@
-number = input('Digite um número de base 2, 10 ou 16:').upper ()
-base = input('O número digitado foi de base 2, 10 ou 16:')
-convert = int(input("Para qual base deseja converter: "))
+number = input('Digite um número de base 2, 8, 10 ou 16:').upper ()
+base = input('O número digitado foi de base 2, 8, 10 ou 16:')
+convert = int(input("Para qual base deseja converter (entre 2, 8, 10 ou 16): "))
 dicionary = {'A':10,"B":11,"C":12,"D":13,"E":14,"F":15,10:'A',11:'B',12:'C',13:"D",14:'E',15:'F'}
-def dectoall (x,y):
+def dectoall (x, y):
     x = int(x)
     binary =[]
     while x>0:
@@ -16,35 +16,25 @@ def dectoall (x,y):
     binary = binary [::-1]
     b = ''.join(map(str, binary))
     return b
-def bintodec(x):
+def othertodec(x, c):
     y = len (x)
     decimal = []
     b = 0
-    for binary in x:
-        binary = int (binary)
-        decimal.append (binary)
-    for indice in range (0, y):
-        b =  b + decimal [indice]*(2**(y - indice - 1))
-    return b
-def hextodec(x):
-    y = len (x)
-    b = 0
-    decimal = []
-    for a in x:
-        if a in dicionary:
-            a = dicionary [a]
-            decimal.append (a)
+    for bases in x:
+        if bases in dicionary:
+            bases = dicionary [bases]
+            decimal.append (bases)
         else:
-            a = int(a)
-            decimal.append (a)
+            bases = int(bases)
+            decimal.append (bases)
     for indice in range (0, y):
-        b =  b + decimal [indice]*(16**(y - indice - 1))
+        b =  b + decimal [indice]*(c**(y - indice - 1))
     return b
 if base == '10':
     r1 = dectoall (number, convert)
     print ('Base', convert, ':', '(', r1,')')
 elif base =='2':
-    r1 = bintodec (number)
+    r1 = othertodec (number)
     r2 = dectoall (r1, convert)
     if convert == 10:
         print ('Base 10: ','(', r1,')')
@@ -53,7 +43,7 @@ elif base =='2':
     else:
         print ("Essa conversão não é possível")
 elif base =='16':
-    r1 = hextodec (number)
+    r1 = othertodec (number)
     r2 = dectoall (r1, convert)
     if convert == 10:
         print ('Base 10: ','(', r1,')')
